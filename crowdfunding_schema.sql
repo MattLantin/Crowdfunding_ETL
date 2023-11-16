4c044a6c006f65612d226907371f67b2319499a4
mcmonte95 <mike.chiaramonte95@gmail.com>
2023-11-15 20:08:26 -0500

    ERD and SQL schema and query cleaned up and completed

--- crowdfunding_schema.sql
+++ crowdfunding_schema.sql
@@ -1,36 +1,46 @@
+-- Drop tables if they already exist
+DROP TABLE IF EXISTS campaign CASCADE;
+DROP TABLE IF EXISTS subcategory CASCADE;
+DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS contacts CASCADE;
+
 -- Create table for contacts data.
 CREATE TABLE contacts (
-	contact_id INT PRIMARY KEY,
-	first_name VARCHAR(15) NOT NULL,
-	last_name VARCHAR(15) NOT NULL,
-	email VARCHAR(45) NOT NULL);
-	
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email_address VARCHAR(50) NOT NULL
+);
+
 -- Create table for category data.
 CREATE TABLE category (
-	category_id VARCHAR(10) PRIMARY KEY,
-	category VARCHAR(20));
-	
+    category_id VARCHAR(30) PRIMARY KEY,
+    category VARCHAR(30)
+);
+
 -- Create table for subcategory data.
 CREATE TABLE subcategory (
-	subcategory_id VARCHAR(10) PRIMARY KEY,
-	subcategory VARCHAR);
+    subcategory_id VARCHAR(30) PRIMARY KEY,
+    subcategory VARCHAR(30)  
+);
 
 -- Create table for campaign data.
 CREATE TABLE campaign (
-	cf_id INT PRIMARY KEY,
-	contact_id INT,
-	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
-	company_name VARCHAR(40) NOT NULL,
-	description VARCHAR(55) NOT NULL,
-	goal DECIMAL(8,2) NOT NULL,
-	pledged DECIMAL(8,2),
-	outcome VARCHAR(10) NOT NULL,
-	backers_count INT,
-	country VARCHAR(2) NOT NULL,
-	currency VARCHAR(3) NOT NULL,
-	launch_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	category_id VARCHAR(5),
-	FOREIGN KEY (category_id) REFERENCES category(category_id),
-	subcategory_id VARCHAR(10),
-	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id));
+    cf_id INT PRIMARY KEY,
+    contact_id INT,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    company_name VARCHAR(30) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR(30) NOT NULL,
+    backers_count INT,
+    country VARCHAR(5) NOT NULL,
+    currency VARCHAR(5) NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(30),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    subcategory_id VARCHAR(30),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
